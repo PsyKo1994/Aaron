59c19d7e31933a5f0a1d428881dbd303890d0532
Phillip Venter <phillip1435@gmail.com>
2021-07-05 17:47:43 +1000

    Feature - Add Aaron as a service

---
+++ ConsoleApp1/AaronService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AaronService
+    {
+
+        public void Aaron()
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace ConsoleApp1
 {
@@ -10,10 +11,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Run as a service
+            // https://www.youtube.com/watch?v=y64L-3HKuP0
+
             //Dapper Dino
             //https://www.youtube.com/watch?v=7-tyLCAO4mY&t=369s
-            var bot = new Bot();
-            bot.RunAsync().GetAwaiter().GetResult();
+
+            //Not service Code
+            //var bot = new Bot();
+            //bot.RunAsync().GetAwaiter().GetResult();
+
+            //Service code
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<AaronService>(s =>
+                {
+                    s.ConstructUsing(AaronService => new AaronService());
+                    s.WhenStarted(AaronService => AaronService.Aaron());
+                    s.WhenStopped(AaronService => AaronService.Aaron());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("Aaron");
+                x.SetDisplayName("Aaron");
+                x.SetDescription("Aaron bot for Discord");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
         }
     }
 }
