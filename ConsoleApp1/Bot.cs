25c298b7656ca80ad4d0d28132d288ce58b9f1ca
Phillip Venter <phillip1435@gmail.com>
2021-11-05 09:59:38 +1000

    BugFix - Added restrictions to Audit Log

--- ConsoleApp1/Bot.cs
+++ ConsoleApp1/Bot.cs
@@ -7,6 +7,7 @@ using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,24 +46,64 @@ namespace ConsoleApp1
             Client.Ready += OnClientReady;
 
             //Logger
+            //Channels for the logger to ignore
+            List<ulong> channelsToIgnore = new List<ulong>();
+            channelsToIgnore.Add(448739263580864522);   //General Announcements
+            channelsToIgnore.Add(448739662438072330);   //T1 Attendance Form
+            channelsToIgnore.Add(862977325640581120);   //T1 Verdicts
+            channelsToIgnore.Add(595178511908732929);   //T2 Attendance Form
+            channelsToIgnore.Add(862977561834422292);   //T2 Verdicts
+            channelsToIgnore.Add(861893058152103966);   //T3 Attendnace Form
+            channelsToIgnore.Add(862978452335099945);   //T3 Verdicts
+            channelsToIgnore.Add(632100047508471809);   //Friday Attendnace Form
+            channelsToIgnore.Add(871696178339786792);   //Friday Verdicts
+            channelsToIgnore.Add(815540017974738986);   //iRacing Attendance Form
+            channelsToIgnore.Add(637979193212141569);   //Hall of Fame
+            channelsToIgnore.Add(604627899542274083);   //T1 Standings
+            channelsToIgnore.Add(604627940503846913);   //T2 Standings
+            channelsToIgnore.Add(873762279575932988);   //T3 Standings
+            channelsToIgnore.Add(597768404002275338);   //Combined Constructors
+            channelsToIgnore.Add(627379648665223168);   //Friday Series Standings
+            channelsToIgnore.Add(755624038772113459);   //License Points
+            channelsToIgnore.Add(682156056301797397);   //Race Incidents Archive
+            channelsToIgnore.Add(874271605864419359);   //Friday Voting
+            channelsToIgnore.Add(749583731701841991);   //T1 Voting
+            channelsToIgnore.Add(874271486892974121);   //T2 Voting
+            channelsToIgnore.Add(874271555415322685);   //T3 Voting
+
             //Log when user deletes a message
             Client.MessageDeleted += async (s, e) =>
             {
                 var author = e.Message.Author;
-                var message = author.Username.ToString() + " Deleted this message: " + e.Message.Content;
-                DiscordChannel channel = e.Guild.GetChannel(904511634834341938);
-                await Client.SendMessageAsync(channel, message).ConfigureAwait(false);
+                
+                //Only log messages not in list
+                if (!channelsToIgnore.Contains(e.Message.ChannelId))
+                {
+                    var message = author.Username.ToString() + " Deleted this message: " + e.Message.Content + "\nFrom this channel: " + e.Message.Channel.Name;
+                    DiscordChannel channel = e.Guild.GetChannel(904511634834341938);
+                    await Client.SendMessageAsync(channel, message).ConfigureAwait(false);
+                }
+
             };
 
             //Log when user modifies a message
             Client.MessageUpdated += async (s, e) =>
             {
                 var author = e.Message.Author;
-                var oldMessage =  "Original Post by " + author.Username.ToString() + " At " + e.MessageBefore.Timestamp + ": \n" + e.MessageBefore.Content;
-                var message = "Edited Post by " + author.Username.ToString() + " At " + e.Message.Timestamp + ": \n" + e.Message.Content;
-                DiscordChannel channel = e.Guild.GetChannel(904511634834341938);
-                await Client.SendMessageAsync(channel, oldMessage).ConfigureAwait(false);
-                await Client.SendMessageAsync(channel, message).ConfigureAwait(false);
+                
+                //Only log messages not in list
+                if (!channelsToIgnore.Contains(e.Message.ChannelId))
+                {
+                    //Only log messages that have actually changed
+                    if (e.Message.Content != e.MessageBefore.Content)
+                    {
+                        var oldMessage = "Original Post by " + author.Username.ToString() + " At " + e.MessageBefore.Timestamp + ": \n" + e.MessageBefore.Content;
+                        var message = "Edited Post by " + author.Username.ToString() + " At " + e.Message.Timestamp + ": \n" + e.Message.Content + "\nIn this channel: " + e.Message.Channel.Name;
+                        DiscordChannel channel = e.Guild.GetChannel(904511634834341938);
+                        await Client.SendMessageAsync(channel, oldMessage).ConfigureAwait(false);
+                        await Client.SendMessageAsync(channel, message).ConfigureAwait(false);
+                    }
+                }
             };
 
             //Interactivity config
